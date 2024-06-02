509d967aa5c6b1690dc981b0b763cc977c65f72d
BiggerH2 <161705584+BiggerH2@users.noreply.github.com>
2024-06-02 15:53:13 +0300

    updated

---
+++ lib/insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 3, 'M', 'Black', 'Playful', 1);
+-- Repeat for other bears
